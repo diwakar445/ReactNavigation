c7ff2ebdf4290c1e50f5537df6092c1d065ba9db
Divakar <divakar@Divakars-MacBook-Air.local>
2025-05-24 14:54:32 +0530

    Added router config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider,createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(
+  [{
+    path:'/',
+    element: <Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      },
+      {
+        path:"contact",
+        element:<Contact/>
+      }
+    ]
+  }]
 )
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <RouterProvider router={router}/>
+);
